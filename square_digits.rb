2e2685ae30f9df46fd4ac5b210300dc149fb87e9
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-11-17 16:25:19 +0100

    add square digit

---
+++ square_digits.rb
@@ -0,0 +1,10 @@
+# TODO : You are asked to square every digit of a number and concatenate them
+
+def square_digits(num)
+  num.digits.reverse.map do |digit|
+    digit * digit
+  end.join.to_i
+end
+
+p square_digits(3212)  # => 9414
+

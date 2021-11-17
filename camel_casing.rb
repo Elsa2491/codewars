f78421e1d094f813bb800c355ecde430eeefeffa
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-11-17 16:24:02 +0100

    add camel casing

---
+++ camel_casing.rb
@@ -0,0 +1,18 @@
+a = 'camelCasing'
+
+
+# METHODE N°1
+def camel_case(string)
+  string.chars.map do |char|
+    char == char.downcase ? char : " #{char}"
+  end.join
+end
+
+
+# METHODE N°2
+def camel_case_with_regex(string)
+  string.gsub(/(?=[A-Z])/, ' ')
+end
+
+p camel_case_with_regex(a)
+p camel_case(a)
